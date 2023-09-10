7731370e5e51928c5e944f6edd453129c211e20e
kw6rcw9 <120956700+kw6rcw9@users.noreply.github.com>
2023-09-10 18:48:59 +0300

    added superuser, migrations, news table

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField('Title', max_length=100, unique=True)
+    text = models.TextField('Main text')
+    date = models.DateTimeField('Date', default=timezone.now)
+    autor = models.ForeignKey(User, verbose_name='Author', on_delete=models.CASCADE)
+
+    views = models.IntegerField('Views', default=1)
+    # sizes = (
+    #     ('S', 'Small'),
+    #     ('M', 'Medium'),
+    #     ('L', 'Large'),
+    # )
+    # shop_sizes = models.CharField(max_length=1, choices=sizes, default='S')
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
+
--- main/views.py
+++ main/views.py
@@ -1,29 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 def home(request):
-    news = [
-        {
-            'title': 'Our first article',
-            'text': 'full text',
-            'date': '1 January 2100',
-            
-
-
-        },
-        {
-            'title': 'Our second article',
-            'text': 'full text',
-            'autor': 'John',
-            'date': '10 January 2100',
-            
-
-
-        }
-    ]
-   
     data = {
-        'news': news,
+        'news': News.objects.all(),
         'title': 'Main page'
     }
     return render(request, 'main/home.html', data)
