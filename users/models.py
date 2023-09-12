e0d0c665a3d21a3d407ae1f11e8b831937241319
kw6rcw9 <120956700+kw6rcw9@users.noreply.github.com>
2023-09-12 15:57:03 +0300

    added user profile, photo upload

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    img = models.ImageField('User`s photo', default='default.png', upload_to='user_images')
+
+    def __str__(self):
+        return f'User`s profile {self.user.username}'
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
