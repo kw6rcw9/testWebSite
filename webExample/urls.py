9dfa78710fc414378821668abfab2b99f50eb5c1
kw6rcw9 <120956700+kw6rcw9@users.noreply.github.com>
2023-10-08 21:17:33 +0300

    added password reset func

--- webExample/urls.py
+++ webExample/urls.py
@@ -28,6 +28,11 @@ urlpatterns = [
     path('profile/', userViews.profile, name='profile'),
     path('user/', authViews.LoginView.as_view(template_name='users/user.html'), name='user'),
     path('exit/', authViews.LogoutView.as_view(template_name='users/exit.html'), name='exit'),
+    path('pass-reset/', authViews.PasswordResetView.as_view(template_name='users/pass_reset.html'), name='pass-reset'),
+    path('password_reset_confirm/<uidb64>/<token>/', 
+         authViews.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_done/', 
+         authViews.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
 ]
 
 if settings.DEBUG:
